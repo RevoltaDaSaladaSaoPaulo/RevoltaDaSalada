ce33f87ae9ace94fda89725787bcb168a13b964b
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2013-06-15 02:04:11 -0300

    celery

--- RevoltaDaSalada/tasks.py
+++ RevoltaDaSalada/tasks.py
@@ -1,6 +1,22 @@
 from celery import task
 import importPost
 
+import djcelery
+
+djcelery.setup_loader()
+BROKER_URL = 'amqp://guest:guest@localhost:5672/'
+
+from datetime import timedelta
+
+CELERYBEAT_SCHEDULE = {
+    'import-every-30-seconds': {
+        'task': 'tasks.import_instagram',
+        'schedule': timedelta(seconds=30)
+    },
+}
+
+CELERY_TIMEZONE = 'UTC'
+
 @task()
 def import_instagram():
     importPost.import_instagram()
