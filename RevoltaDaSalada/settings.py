329232523addacafe75080c80c2bf729875e0ef4
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2013-06-15 23:15:58 -0300

    fix

--- RevoltaDaSalada/settings.py
+++ RevoltaDaSalada/settings.py
@@ -8,7 +8,6 @@ BROKER_URL = 'amqp://guest@127.0.0.1:5672/'
 from datetime import timedelta
 
 CELERY_RESULT_BACKEND = "amqp"
-CELERY_IMPORTS = ("tasks.import_instagram", )
 
 CELERYBEAT_SCHEDULE = 'djcelery.schedulers.DatabaseScheduler'
 
