86171a6bf70d55188b7aa7a3631ade856a9f9369
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2013-06-16 04:48:45 -0300

    fix

--- RevoltaDaSalada/models.py
+++ RevoltaDaSalada/models.py
@@ -26,9 +26,7 @@ class InheritanceCastModel(models.Model):
     class Meta:
         abstract = True
 
-class Post(InheritanceCastModel):
-    id = models.BigIntegerField(primary_key=True)
-    description = models.TextField(max_length=500)
+class Post(InheritanceCastModel):    description = models.TextField(max_length=500)
     author = models.CharField(max_length=80)
     author_thumbnail_url = models.CharField(max_length=500)
     url = models.URLField()
--- RevoltaDaSalada/settings.py
+++ RevoltaDaSalada/settings.py
@@ -3,13 +3,9 @@ import os
 import djcelery
 
 djcelery.setup_loader()
-BROKER_URL = 'amqp://guest@127.0.0.1:5672/'
 
 from datetime import timedelta
 
-BROKER_URL = 'redis://localhost:6379/0'
-BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}  # 1 hour.
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 CELERYBEAT_SCHEDULE = 'djcelery.schedulers.DatabaseScheduler'
 
